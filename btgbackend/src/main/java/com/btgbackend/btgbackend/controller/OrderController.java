123c82475d4044703381687cf2d16ff9141f8eba
anthonyMeds <anthony.eduardo@hotmail.com>
2024-06-23 16:55:13 -0300

    feat: buscar lista de pedidos controller

--- btgbackend/src/main/java/com/btgbackend/btgbackend/controller/OrderController.java
+++ btgbackend/src/main/java/com/btgbackend/btgbackend/controller/OrderController.java
@@ -1,2 +1,28 @@
-package com.btgbackend.btgbackend.controller;public class OrderController {
+package com.btgbackend.btgbackend.controller;
+
+import com.btgbackend.btgbackend.controller.dto.ApiResponse;
+import com.btgbackend.btgbackend.controller.dto.OrderResponse;
+import com.btgbackend.btgbackend.service.OrderService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/customers/{customerId}/orders")
+    public ResponseEntity<ApiResponse<OrderResponse>> listOrders(@RequestParam(name = "page", defaultValue = "0") Integer page,
+                                                                 @RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize) {
+
+        return ResponseEntity.ok(null );
+    }
+
+
 }
