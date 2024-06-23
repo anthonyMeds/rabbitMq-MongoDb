bd40c66092dd1afc83ea5bba7c54c1f921cf5add
anthonyMeds <anthony.eduardo@hotmail.com>
2024-06-23 14:26:39 -0300

    Setup do projeto

---
+++ btgbackend/src/main/java/com/btgbackend/btgbackend/BtgbackendApplication.java
@@ -0,0 +1,13 @@
+package com.btgbackend.btgbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtgbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtgbackendApplication.class, args);
+	}
+
+}
---
+++ btgbackend/src/test/java/com/btgbackend/btgbackend/BtgbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btgbackend.btgbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtgbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
